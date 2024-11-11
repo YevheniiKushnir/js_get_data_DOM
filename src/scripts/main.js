6ce5703904eeb463c3ad22160f42e0e7c8ef0ddf
Yevhenii Kushnir <aroblack17@gmail.com>
2024-11-11 15:54:56 +0100

    Top 10 largest countries

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const population = [...document.querySelectorAll('span.population')].map(
+  (el) => +el.textContent.replace(/,/g, ''),
+);
+
+const total = population.reduce((a, b) => a + b);
+const average = Math.floor(total / population.length);
+
+const replaceCalculateIt = (classSelector, value) => {
+  document.querySelector(classSelector).textContent =
+    value.toLocaleString('en-US');
+};
+
+replaceCalculateIt('.average-population', average);
+replaceCalculateIt('.total-population', total);
